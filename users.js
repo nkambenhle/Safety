f6fa0b925e98589a218ec256e3ecdc45e919af9d
Nkambenhle Zungu <nkambenhle.zungu@digiata.com>
2026-02-03 22:21:51 +0200

    Fix middleware imports

--- users.js
+++ users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { createClient } = require('@supabase/supabase-js');
-const authMiddleware = require('../middleware/auth');
+const authMiddleware = require('./middleware/auth.js');
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
